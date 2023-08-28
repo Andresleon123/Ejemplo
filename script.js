d8f7c8e2a57d1ccc9deef01275a8d03ae7661932
Andresleon123 <101126916+Andresleon123@users.noreply.github.com>
2023-08-28 09:11:44 -0500

    Borrador

---
+++ script.js
@@ -0,0 +1,18 @@
+itemForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre=productos = document.getElementById('nombre-productos').value;
+    const precio = parseFloat(document.getElementById('precio').value);
+    const cantidad = parseInt(document.getElementById('cantidad').value);
+
+    const itemRow = document.createElement('div');
+    itemRow.classList.add('item-row');  
+    itemRow.innerHTML = `
+        <strong>${nombre-productos}</strong>
+        <p>Price: $${precio.toFixed(2)}</p>
+        <p>Quantity: ${cantidad}</p>
+    `;
+        LEONContainer.appendChild(itemRow);
+
+        itemForm.reset();
+});
